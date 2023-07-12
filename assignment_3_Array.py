df88bf27ba567ffd28ae920933aca60fe44eb432
Bhawna011 <107423783+Bhawna011@users.noreply.github.com>
2023-07-12 16:20:23 +0530

    Update assignment_3_Array.py

--- assignment_3_Array.py
+++ assignment_3_Array.py
@@ -50,7 +50,7 @@ print(ans[0])
 # Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
 
 
-# 💡 **Question 3**
+#  **Question 3**
 # A permutation of an array of integers is an arrangement of its members into a
 # sequence or linear order.
 
